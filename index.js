b708b2d5b96ae965d6c2d6a4264036277246f682
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-14 14:30:39 -0400

    Added a little bit more features

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 console.clear();
-const b = document.querySelector('button');
+const b1 = document.querySelector('#b1');
+const b2 = document.querySelector('#b2');
 
 function changeTheText(){
-    const text = document.querySelector('.hi');
     const inputtedText = document.getElementById("inputting").value;
-    text.textContent = inputtedText;
-
+    const num = document.getElementById("inputting2").value;
+    if(num == "1"){
+        const text = document.querySelector('.hi1');
+        text.textContent = "1- " + inputtedText;
+    } else if(num == "2"){
+        const text = document.querySelector('.hi2');
+        text.textContent = "2- " + inputtedText;
+    } else if(num == "3"){
+        console.log(num);
+        const text = document.querySelector('.hi3');
+        text.textContent = "3- " + inputtedText;
+    } else if(num == "4"){
+        const text = document.querySelector('.hi4');
+        text.textContent = "4- " + inputtedText;
+    }else if(num == "5"){
+        const text = document.querySelector('.hi5');  
+        text.textContent = "5- " + inputtedText; 
+    } else{
+        window.alert("Invalid number!");
+    }
 }
 
-b.addEventListener('click', changeTheText);
+function clearText(){
+    document.getElementById("inputting").value = "";
+    document.getElementById("inputting2").value = "";
+}
+
+b1.addEventListener('click', changeTheText);
+b2.addEventListener('click', clearText);
+
