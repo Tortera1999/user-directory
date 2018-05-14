b303ebd008b57bda29512ee483b70b44fdaa0b96
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-14 13:36:23 -0400

    HW complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('it works')
+const b = document.querySelector('button');
+
+function changeTheText(){
+    let text = document.querySelector('h1');
+    text.textContent = 'yo!'
+}
+
+b.addEventListener('click', changeTheText)
