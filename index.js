7bcfb931c19ae5508e4a94075f577d2f36172154
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-15 10:42:04 -0400

    Add multiple names, clearing input each time

--- index.js
+++ index.js
@@ -7,25 +7,29 @@ function changeTheText(ev){
     ev.preventDefault();
     const inputtedText = document.getElementById("inputting").value;
     const num = document.getElementById("inputting2").value;
-    if(num == "1"){
-        const text = document.querySelector('.hi1');
-        text.textContent = "1- " + inputtedText;
-    } else if(num == "2"){
-        const text = document.querySelector('.hi2');
-        text.textContent = "2- " + inputtedText;
-    } else if(num == "3"){
-        console.log(num);
-        const text = document.querySelector('.hi3');
-        text.textContent = "3- " + inputtedText;
-    } else if(num == "4"){
-        const text = document.querySelector('.hi4');
-        text.textContent = "4- " + inputtedText;
-    }else if(num == "5"){
-        const text = document.querySelector('.hi5');  
-        text.textContent = "5- " + inputtedText; 
-    } else{
-        window.alert("Invalid number!");
-    }
+    // if(num == "1"){
+    //     const text = document.querySelector('.hi1');
+    //     text.textContent = "1- " + inputtedText;
+    // } else if(num == "2"){
+    //     const text = document.querySelector('.hi2');
+    //     text.textContent = "2- " + inputtedText;
+    // } else if(num == "3"){
+    //     console.log(num);
+    //     const text = document.querySelector('.hi3');
+    //     text.textContent = "3- " + inputtedText;
+    // } else if(num == "4"){
+    //     const text = document.querySelector('.hi4');
+    //     text.textContent = "4- " + inputtedText;
+    // }else if(num == "5"){
+    //     const text = document.querySelector('.hi5');  
+    //     text.textContent = "5- " + inputtedText; 
+    // } else{
+    //     window.alert("Invalid number!");
+    // }
+
+    const text = document.querySelector('#users')
+    text.textContent += (" " + inputtedText)
+    document.getElementById("inputting").value = "";
 }
 
 function clearText(ev){
