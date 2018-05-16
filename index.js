6d287131395488bb9d2f479087fc040084245158
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-16 11:13:35 -0400

    Extract render list item

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const handleSubmit = function(ev) {
 
   const users = document.querySelector('#users')
 
-  const list = document.createElement('ul')
-
-  Object.keys(user).forEach(function(label){
-    list.appendChild(renderListItem(label, user[label]))
-  })
-
-  users.appendChild(list)
+  users.appendChild(renderList(user))
 
   form.reset()
   form.userName.focus()
@@ -28,6 +22,14 @@ form.addEventListener('submit', handleSubmit)
 
 const clearList = document.querySelector('#b3')
 clearList.addEventListener('click', clearTheList)
+
+function renderList(data){
+    const list = document.createElement('ul')
+    Object.keys(data).forEach(function(label){
+        list.appendChild(renderListItem(label, data[label]))
+      })
+    return list
+}
 
 function renderListItem(label, value) {
     const item = document.createElement('li')
@@ -41,14 +43,6 @@ function renderListItem(label, value) {
 }
  
 function renderColor(color){
-    // const colorDiv = document.createElement('div')
-    // const favoriteColor = form.favoriteColor.value
-    // colorDiv.style.backgroundColor = favoriteColor
-    // colorDiv.style.width = '6rem'
-    // colorDiv.style.height = '3rem'
-    // colorDiv.style.marginLeft = '0%'
-    // return colorDiv
-
     const colorDiv = document.createElement('div')
     colorDiv.style.backgroundColor = color
     colorDiv.style.width = '6rem'
