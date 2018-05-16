d040b00cb41975f934b538d71f079c76bde74ac6
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-05-16 10:06:32 -0400

    Modified code

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('form#userForm')
 
 const handleSubmit = function(ev) {
-  ev.preventDefault()
+    ev.preventDefault()
+  const form = ev.target
+  const userName = form.userName.value
+  const age = form.age.value
+  const favoriteColor = renderColor(form.favoriteColor.value)
 
   const users = document.querySelector('#users')
-  users.appendChild(renderList())
+
+  const list = document.createElement('ul')
+
+  list.appendChild(renderListItem('Name', userName))
+  list.appendChild(renderListItem('Age', age))
+  list.appendChild(renderListItem('Favorite Color', favoriteColor))
+
+  users.appendChild(list)
 
   form.reset()
   form.userName.focus()
@@ -15,39 +26,51 @@ form.addEventListener('submit', handleSubmit)
 const clearList = document.querySelector('#b3')
 clearList.addEventListener('click', clearTheList)
 
-function renderList(){
-    const userName = form.userName.value
-    const age = form.age.value
+// function renderList(){
+//     const userName = form.userName.value
+//     const age = form.age.value
 
-    const list = document.createElement('ul')
+//     const list = document.createElement('ul')
 
-    const nameItem = renderListItem(`Name: ${userName}`)
-    const ageItem = renderListItem(`Age: ${age}`)
-    const colorItem = renderListItem('Favorite Color: ')
-    colorItem.appendChild(renderColorDiv())
-  
-    list.appendChild(nameItem)
-    list.appendChild(ageItem)
-    list.appendChild(colorItem)
+//     const colorItem = document.createElement('li')
+//     colorItem.appendChild(renderColorDiv())
 
-    return list
-}
+//     list.appendChild(renderListItem('Name', userName))
+//     list.appendChild(renderListItem('Age', age))
+//     list.appendChild(renderListItem('Favorite Color', colorItem))
 
-function renderListItem(x){
-    const Y = document.createElement('li')
-    Y.textContent = x
-    return Y;
+//     return list
+// }
+
+
+function renderListItem(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: `
+    try {
+      item.appendChild(value)
+    } catch(e) {
+      item.textContent += value
+    }
+    return item
 }
  
-function renderColorDiv(){
+function renderColor(color){
+    // const colorDiv = document.createElement('div')
+    // const favoriteColor = form.favoriteColor.value
+    // colorDiv.style.backgroundColor = favoriteColor
+    // colorDiv.style.width = '6rem'
+    // colorDiv.style.height = '3rem'
+    // colorDiv.style.marginLeft = '0%'
+    // return colorDiv
+
     const colorDiv = document.createElement('div')
-    const favoriteColor = form.favoriteColor.value
-    colorDiv.style.backgroundColor = favoriteColor
+    colorDiv.style.backgroundColor = color
     colorDiv.style.width = '6rem'
     colorDiv.style.height = '3rem'
     colorDiv.style.marginLeft = '0%'
     return colorDiv
 }
+
 
 function clearTheList(){
     (document.querySelector('#users')).remove()
